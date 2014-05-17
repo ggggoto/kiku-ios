253f41f9b737c4295846759d944754552cc83581
TsuzukiTomoaki <tsuzukitomoaki@TsuzukiTomoaki-no-MacBook-Pro.local>
2014-05-18 01:20:38 +0900

    Parse

---
+++ kiku/Classes/macro/Macro.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  kiku
+//
+//  Created by TsuzukiTomoaki on 2014/05/18.
+//  Copyright (c) 2014年 TsuzukiTomoaki. All rights reserved.
+//
+
+#ifndef kiku_Header_h
+#define kiku_Header_h
+
+
+
+#endif
