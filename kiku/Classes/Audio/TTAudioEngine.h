504e8ba008f61e302efb4b494a2d1d7737326e9f
TsuzukiTomoaki <tsuzukitomoaki@TsuzukiTomoaki-no-MacBook-Pro.local>
2014-05-18 05:08:31 +0900

    Audio Engine brush up

--- kiku/Classes/Audio/TTAudioEngine.h
+++ kiku/Classes/Audio/TTAudioEngine.h
@@ -10,11 +10,34 @@
 #import <AVFoundation/AVFoundation.h>
 #import <MediaPlayer/MediaPlayer.h>
 
+@protocol TTAudioEngineDelegate
+-(void)readyPlaying;
+-(void)updateCurrentPlaybackTime:(int)currentPlaybackTime;
+@end
+
 @interface TTAudioEngine : NSObject<AVAudioPlayerDelegate>
 
-@property (nonatomic, retain) AVAudioPlayer *audioPlayer;
-@property (nonatomic, retain) MPMoviePlayerController *mediaPlayer;
+typedef enum {
+    kAudioDoNotRepeat,
+    kAudioSongRepeat,
+    kAudioAlbumRepeat,
+} TTAudioEngineMode;
 
+@property (nonatomic, strong) id<TTAudioEngineDelegate> delegate;
+@property (nonatomic, strong) AVAudioPlayer *audioPlayer;
+@property (nonatomic, strong) MPMoviePlayerController *mediaPlayer;
+@property (nonatomic, assign) TTAudioEngineMode mode;
+@property (nonatomic, strong) NSString *currentPlayingUrl;
+
+- (void)setMode:(TTAudioEngineMode)mode;
 - (void)play:(NSString*)urlStr;
+- (int)getCurrentPlaybackDuration;
+- (void)seek:(int)time;
+- (void)setLockScreenInfo:(UIImage*)art
+               audioTitle:(NSString*)audioTitle
+               albumTitle:(NSString*)albumTitle
+                   artist:(NSString*)artist;
+- (void)enque:(NSString*)urlStr; //Add info too
+- (void)deque:(NSString*)urlStr;
 
 @end
--- kiku/Classes/Audio/TTAudioEngine.m
+++ kiku/Classes/Audio/TTAudioEngine.m
@@ -7,70 +7,141 @@
 //
 
 #import "TTAudioEngine.h"
+#import "Macro.h"
 
 @implementation TTAudioEngine
 
+@synthesize delegate = _delegate;
 @synthesize audioPlayer = _audioPlayer;
 @synthesize mediaPlayer = _mediaPlayer;
+@synthesize mode = _mode;
+@synthesize currentPlayingUrl = _currentPlayingUrl;
+
+- (id)init {
+    self = [super init];
+    _mode = kAudioDoNotRepeat;
+    _currentPlayingUrl = NULL;
+    return self;
+}
 
 - (void)play:(NSString*)urlStr {
     
     [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
     
     _mediaPlayer = [[MPMoviePlayerController alloc] initWithContentURL:[NSURL URLWithString: urlStr]];
-    _mediaPlayer.shouldAutoplay = NO;
-    _mediaPlayer.scalingMode= MPMovieScalingModeFill;
-    _mediaPlayer.controlStyle =MPMovieControlStyleNone;
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:MPMoviePlayerPlaybackDidFinishNotification object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(null)
-                                                 name:MPMoviePlayerPlaybackDidFinishNotification
-                                               object:nil];
     
-    //MPMediaItemArtwork *albumArt = [[MPMediaItemArtwork alloc] initWithImage: [UIImage imagedNamed:@"AlbumArt"]];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:MPMoviePlayerLoadStateDidChangeNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(changedLoadState:)
+                                                 name:MPMoviePlayerLoadStateDidChangeNotification
+                                               object:nil];
     
     AVAudioSession* audioSession = [AVAudioSession sharedInstance];
     NSError* error = nil;
     [audioSession setCategory:AVAudioSessionCategoryPlayback error:&error];
     [audioSession setActive:YES error:&error];
+ 
+    [_mediaPlayer setShouldAutoplay:NO];
+    [_mediaPlayer setControlStyle:MPMovieControlStyleNone];
+    [_mediaPlayer play];
+    _currentPlayingUrl = urlStr;
     
+    [NSTimer scheduledTimerWithTimeInterval:1 target:[NSBlockOperation blockOperationWithBlock:^{
+        int time = _mediaPlayer.currentPlaybackTime;
+        [_delegate updateCurrentPlaybackTime:time];
+    }] selector:@selector(main) userInfo:nil repeats:YES];
+}
+
+- (void)setMode:(TTAudioEngineMode)mode {
+    _mode = mode;
+}
+
+- (int)getCurrentPlaybackDuration {
+    if (_mediaPlayer.playbackState != MPMoviePlaybackStatePlaying
+        || _mediaPlayer.playbackState != MPMoviePlaybackStatePaused) {
+        return 0;
+    }
+    return _mediaPlayer.duration;
+}
+
+- (void)seek:(int)time {
+    if (_mediaPlayer.playbackState != MPMoviePlaybackStatePlaying
+        || _mediaPlayer.playbackState != MPMoviePlaybackStatePaused) {
+        return;
+    }
+    if (time > [self getCurrentPlaybackDuration] && time > 0) {
+#ifdef DEBUG_OUT
+        NSLog(@"Seeking without duration");
+#endif
+        return;
+    }
+    [_mediaPlayer setCurrentPlaybackTime:time];
+}
+
+- (void)setLockScreenInfo:(UIImage*)art
+               audioTitle:(NSString*)audioTitle
+               albumTitle:(NSString*)albumTitle
+                   artist:(NSString*)artist {
+    MPMediaItemArtwork *albumArt = [[MPMediaItemArtwork alloc] initWithImage:art];
     NSMutableDictionary *songInfo = [[NSMutableDictionary alloc] init];
     [songInfo setObject:@"Audio Title" forKey:MPMediaItemPropertyTitle];
     [songInfo setObject:@"Audio Author" forKey:MPMediaItemPropertyArtist];
     [songInfo setObject:@"Audio Album" forKey:MPMediaItemPropertyAlbumTitle];
+    [songInfo setObject:albumArt forKey:MPMediaItemPropertyArtwork];
     [MPNowPlayingInfoCenter defaultCenter].nowPlayingInfo = songInfo;
-    
-    [_mediaPlayer play];
-    /*
-    dispatch_queue_t dispatchQueue =
-    dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-    dispatch_async(dispatchQueue, ^(void){
-        NSString *_mp3file = urlStr;
-        NSData   *_mp3Data = [NSData dataWithContentsOfURL:[NSURL URLWithString: _mp3file]];
-        NSError  *_error   = nil;
-        
-        [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
-        
-        _audioPlayer = [[AVAudioPlayer alloc] initWithData:_mp3Data error:&_error];
+}
 
-        AVAudioSession* audioSession = [AVAudioSession sharedInstance];
-        NSError* error = nil;
-        [audioSession setCategory:AVAudioSessionCategoryPlayback error:&error];
-        [audioSession setActive:YES error:&error];
+- (void) changedLoadState:(NSNotification*)notification
+{
+    MPMoviePlayerController *mediaPlayer = [notification object];
+    
+    if ([mediaPlayer loadState] != MPMovieLoadStateUnknown) {
+
+        [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                        name:MPMoviePlayerLoadStateDidChangeNotification
+                                                      object:nil];
         
-        if (_audioPlayer != nil)
-        {
-            if ([_audioPlayer prepareToPlay] && [_audioPlayer play])
-            {
-                NSLog(@"Successfully started playing");
-            } else {
-                NSLog(@"Failed to play");
-            }
-        } else {
-            NSLog(@"Failed to instanciate player");
-        }
-    });
-     */
+        [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                        name:MPMoviePlayerTimedMetadataUpdatedNotification
+                                                      object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(finishedPlay:)
+                                                     name:MPMoviePlayerPlaybackDidFinishNotification
+                                                   object:nil];
+        
+        [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                        name:MPMoviePlayerNowPlayingMovieDidChangeNotification
+                                                      object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(changedState:)
+                                                     name:MPMoviePlayerNowPlayingMovieDidChangeNotification
+                                                   object:mediaPlayer];
+        
+        [_delegate readyPlaying];
+    }
+}
+
+- (void)changedState:(NSNotification*)notificaiton {
+    //MPMoviePlayerController *mediaPlayer = [notificaiton object];
+}
+
+- (void)finishedPlay:(NSNotification*)notification {
+#ifdef DEBUG_OUT
+    NSLog(@"play finished");
+#endif
+    switch (_mode) {
+        case kAudioDoNotRepeat:
+            break;
+        case kAudioSongRepeat:
+            [self play:_currentPlayingUrl];
+            break;
+        case kAudioAlbumRepeat:
+            break;
+        default:
+            @throw @"Unclassified mode";
+            break;
+    }
+    
 }
 
 @end
